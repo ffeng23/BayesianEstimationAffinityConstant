723f5a1c1a8397500d6e3339412e43a184d83d3a
Feng <ffeng23@gmail.com>
2013-10-21 16:04:33 -0400

    This is the branch for the first release. We did not add/modify anything compared to the master branch, but instead we only removed the debugging comments on it.

--- testingNelderMead1D/testingNelderMead1D.cs
+++ testingNelderMead1D/testingNelderMead1D.cs
@@ -7,6 +7,7 @@ using BayesianEstimateLib;
 
 namespace testingNelderMead1D
 {
+    //public delegate double LogDistributionFuctionDelegate(double x, double functionNormConstant);
     class testingNelderMead1D
     {
         static void Main(string[] args)
@@ -19,7 +20,7 @@ namespace testingNelderMead1D
             for (int i = 0; i < count; i++)
             {
                 x.Add(low + i * 0.5);
-                y.Add(logNormalDist(x[i]));
+                y.Add(logNormalDist(x[i], 0));
             }
 
             //now write the output
@@ -27,8 +28,9 @@ namespace testingNelderMead1D
             DataIO.WriteDataTable(x, y, "normalDist.txt",  new List<string> { "x", "y" });
             Console.WriteLine("Done............");
 
+            double funcNormalConst = 0;
             Console.WriteLine("start testing nelder mead algorithm..........");
-            double nonzero=NelderMead1D.FindNonZeroValue(logNormalDist, 0, 3000, 0, 3000, -5);
+            double nonzero = NelderMead1D.FindNonZeroValue(logNormalDist, 0, 3000, 0, 3000, NelderMeadMethod.NelderMead1D.LOG_LIMIT, ref funcNormalConst);
             Console.WriteLine("the nonzero value is " + nonzero);
             Console.WriteLine("doing............");
 
@@ -39,8 +41,17 @@ namespace testingNelderMead1D
 
 
         //define a normal distribution for testing
-
-        static double logNormalDist(double _x)
+        /// <summary>
+        /// this is method for testing
+        /// </summary>
+        /// <param name="_x">the input for doing logNormal</param>
+        /// <param name="normalConst">this is the constant to normalize the result. we actually do not use this for the actual function
+        ///             We use this only for compatibility issue. since NelderMead1D.FindNonZerValue needs this form of function to work
+        ///             For this function logNormal, it is easy and no need for this. But for other complicated functions, we need this
+        ///             constant to normalize the output and find the value.
+        /// </param>
+        /// <returns></returns>
+        static double logNormalDist(double _x, double normalConst)
         {
             double y;
             double mu = 2500, sigma = 0.1;
